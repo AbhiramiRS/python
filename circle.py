3380f112542d49c65a5a4b8afda11bb0aea513c5
Abhirami Rajesh <97380115+AbhiramiRS@users.noreply.github.com>
2022-02-28 22:17:08 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,6 @@
+from math import pi
+r=float(input("enter radius"))
+area=(pi*r*r)
+perimeter=(2*pi*r)
+print("area of the circle is:","%.2f"%area)
+print("perimeter of acircle is:","%.2f"%perimeter)
