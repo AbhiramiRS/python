a4fae17ffa1aafc9d017fca03e414035b77ed661
Abhirami Rajesh <97380115+AbhiramiRS@users.noreply.github.com>
2022-02-28 23:29:37 +0530

    Add files via upload

---
+++ fibonnaci.py
@@ -0,0 +1,15 @@
+ef fib(n):
+    if (n<=1):
+        return n
+    else:
+        return (fib(n-1)+fib(n-2))
+
+n=int(input("\nEnter the limit : "))
+
+if(n<=0):
+    print("\nenter positive number")
+else:
+    print("\nFibnoacci series : ")
+    for i in range(n):
+        print(fib(i))
+© 2022 GitHub, Inc.
